2b516cb8ac7814ca404bb796fd72520cd879c930
Wang Jianing <Evilrabbit520@Gmail.com>
2025-02-19 02:08:00 +0800

    Reconfigured sitemap.

---
+++ overrides/javascripts/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6bf791d3f062836cb2d33f01916ab811";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
